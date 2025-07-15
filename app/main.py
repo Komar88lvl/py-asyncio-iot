ddadb47d5be8b6f81af4215be1fe4f9f435a52ab
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-07-15 15:21:31 +0300

    Change wake_up_program to async

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 import time
 import asyncio
 
+from typing import Any, Awaitable
+
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
+
+
+async def run_sequence(*functions: Awaitable[Any]) -> None:
+    for function in functions:
+        await function
+
+
+async def run_parallel(*functions: Awaitable[Any]) -> None:
+    await asyncio.gather(*functions)
 
 
 async def main() -> None:
@@ -22,15 +33,13 @@ async def main() -> None:
     hue_light_id, speaker_id, toilet_id = device_ids
 
         # create a few programs
-    wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(
-            speaker_id,
-            MessageType.PLAY_SONG,
-            "Rick Astley - Never Gonna Give You Up"
-        ),
-    ]
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+        run_sequence(
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
+            service.send_msg(Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"))
+        )
+    )
 
     sleep_program = [
         Message(hue_light_id, MessageType.SWITCH_OFF),
@@ -40,7 +49,7 @@ async def main() -> None:
     ]
 
     # run the programs
-    await service.run_program(wake_up_program)
+    # await service.run_program(wake_up_program)
     await service.run_program(sleep_program)
 
 
